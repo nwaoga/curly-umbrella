d0ca22b2a28fb1ca09a52e8112694c0eeb1442f0
Eric <ericn@sfwltd.co.uk>
2018-05-05 23:11:17 +0100

    initial Stadium and team table created

---
+++ FootieWorld/FootieWorld.database/dbo/Tables/tblStadium.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblStadium]
+(
+	[StadiumID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(255) NULL, 
+    [Address1] VARCHAR(255) NULL, 
+    [Address2] VARCHAR(255) NULL, 
+    [Address3] VARCHAR(255) NULL, 
+    [Address4] VARCHAR(255) NULL, 
+    [Capacity] INT NULL 
+)
---
+++ FootieWorld/FootieWorld.database/dbo/Tables/tblTeam.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblTeam] (
+    [TeamId]          UNIQUEIDENTIFIER NOT NULL,
+    [Name]        VARCHAR (255)    NOT NULL,
+    [NickName]    VARCHAR (255)    NULL,
+    [Address1]    VARCHAR (255)    NULL,
+    [Address2]    VARCHAR (255)    NULL,
+    [Address3]    VARCHAR (255)    NULL,
+    [Address4]    VARCHAR (255)    NULL,
+    [Postcode]    VARCHAR (255)    NULL,
+    [Colours]     VARCHAR (255)    NULL,
+    [PhoneNumber] VARCHAR (255)    NULL,
+    [Website]     VARCHAR (255)    NULL,
+    [StadiumID] UNIQUEIDENTIFIER NULL, 
+    CONSTRAINT [PK_tblteam] PRIMARY KEY CLUSTERED ([TeamId] ASC), 
+    CONSTRAINT [FK_tblTeam_ToStadium] FOREIGN KEY (StadiumID) REFERENCES [tblStadium](StadiumID)
+);
+
