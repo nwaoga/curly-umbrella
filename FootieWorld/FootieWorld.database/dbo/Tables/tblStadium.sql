f99f1e9231004bb2e3642f04ecefe13340bd0170
Eric <ericn@sfwltd.co.uk>
2018-05-16 09:19:17 +0100

    updated tables

--- FootieWorld/FootieWorld.database/dbo/Tables/tblStadium.sql
+++ FootieWorld/FootieWorld.database/dbo/Tables/tblStadium.sql
@@ -1,10 +1,14 @@
-﻿CREATE TABLE [dbo].[tblStadium]
-(
-	[StadiumID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(255) NULL, 
-    [Address1] VARCHAR(255) NULL, 
-    [Address2] VARCHAR(255) NULL, 
-    [Address3] VARCHAR(255) NULL, 
-    [Address4] VARCHAR(255) NULL, 
-    [Capacity] INT NULL 
-)
+﻿CREATE TABLE [dbo].[tblStadium] (
+    [StadiumID] UNIQUEIDENTIFIER NOT NULL,
+    [Name]      VARCHAR (255)    NULL,
+    [Address1]  VARCHAR (255)    NULL,
+    [Address2]  VARCHAR (255)    NULL,
+    [Address3]  VARCHAR (255)    NULL,
+    [Address4]  VARCHAR (255)    NULL,
+    [Capacity]  INT              NULL,
+    [Postcode]  NCHAR (10)       NULL,
+    [Nickname]  VARCHAR (255)    NULL,
+    PRIMARY KEY CLUSTERED ([StadiumID] ASC)
+);
+
+
